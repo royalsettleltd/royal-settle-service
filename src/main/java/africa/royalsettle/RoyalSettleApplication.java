25a6ec49253864d5261282ed0014d12fa9dfe900
Lekan Sofuyi <lekan.sofuyi01@gmail.com>
2025-11-11 15:47:23 +0100

    Initial Commit to Royal Settle Application

---
+++ src/main/java/africa/royalsettle/RoyalSettleApplication.java
@@ -0,0 +1,12 @@
+package africa.royalsettle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalSettleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoyalSettleApplication.class, args);
+    }
+}
